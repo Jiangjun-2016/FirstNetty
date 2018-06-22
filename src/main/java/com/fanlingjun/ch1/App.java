f6361a31e1a27414d505fa697f611771a8ee7976
fanxiangfeng <295392867@qq.com>
2018-06-22 13:46:49 +0800

    netty

--- src/main/java/com/fanlingjun/ch1/App.java
+++ src/main/java/com/fanlingjun/ch1/App.java
@@ -2,10 +2,10 @@ package com.fanlingjun.ch1;
 
 /**
  * Hello world!
- *
  */
 public class App {
-    public static void main( String[] args ){
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
     }
 }
